3697d4c6b132510bffda3617ffba499442a37ed2
Yuji Iwanaga <nm7.ty.nt.abc@gmail.com>
2024-08-03 20:28:40 +0900

    [pattern1] refactor: no use pointer.

--- src/pattern1/main.cpp
+++ src/pattern1/main.cpp
@@ -8,19 +8,14 @@ using namespace design_pattern;
 
 int main(int argc, char* argv[])
 {
-  File* file = new File(0, "file1.txt");
-  Directory* dir = new Directory(0, "directory1");
-  dir->add(file);
-  std::list<Node*> pResult = dir->find("file1.txt");
-  for (auto it = pResult.begin(); it != pResult.end();)
+  File file(0, "file1.txt");
+  Directory dir(0, "directory1");
+  dir.add(&file);
+  auto pResult = dir.find("file1.txt");
+  for (auto& node : pResult)
   {
-    std::cout << "find name :" << (*it)->getName() << std::endl;
-    it++;
+    std::cout << "find name :" << node->getName() << std::endl;
   }
 
-  delete file;
-  file = nullptr;
-  delete dir;
-  dir = nullptr;
   return 0;
 }
